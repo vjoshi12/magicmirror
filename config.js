25fe43b11f1a61969114973bf4f3bbb66e47a1f8
vjoshi12 <vjoshi12@gmail.com>
2015-04-25 14:45:24 -0700

    Second commit

---
+++ config.js
@@ -0,0 +1,16 @@
+var currWeatherUrl = "http://api.openweathermap.org/data/2.5/weather";
+var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast";
+
+var currWeatherParams = {
+	"zip":"98101",
+	"units":"imperial"
+};
+
+var forecastParams = {
+	"q":"Seattle,us",
+	"mode":"xml",
+	"units":"imperial"
+};
+
+
+
---
+++ main.js
@@ -0,0 +1,30 @@
+jQuery.fn.updateWithText = function(text, speed) {
+	var dummy = $("<h1/>").html(text);
+	if ($(this).html() != dummy.html()) {
+		$(this).fadeOut(speed / 2, function() {
+			$(this).html(text);
+			$(this).fadeIn(speed / 2, function(){});
+		});
+	}
+}		
+			
+function roundVal(val) {
+	return Math.round(val * 10) / 10;
+}
+
+$(document).ready(function() {
+
+	// Update the time
+	window.setInterval(function() {
+		$("#displayDate").html(moment().format("dddd, MMMM Do YYYY"));
+		$("#displayTime").updateWithText(moment().format("hh:mm"), 1000);
+	}, 10000);
+
+	// Update the weather
+	window.setInterval(function() {
+		$.getJSON(currWeatherUrl, currWeatherParams, function(json, t) {
+			var temp = roundVal(json.main.temp) + "&deg;";
+			$("#currWeather").updateWithText(temp, 1000);
+		});
+	}, 60000);
+});
