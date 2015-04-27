c98776ce22328df18c2327449d31bd6d56faf22a
vjoshi12 <vjoshi12@gmail.com>
2015-04-26 23:25:06 -0700

    Add wind/sun info

--- js/main.js
+++ js/main.js
@@ -40,9 +40,44 @@ $(document).ready(function() {
 
 	// Update the weather
 	window.setInterval(function() {
+		var iconTable = {
+			'01d':'wi-day-sunny',
+			'02d':'wi-day-cloudy',
+			'03d':'wi-cloudy',
+			'04d':'wi-cloudy-windy',
+			'09d':'wi-showers',
+			'10d':'wi-rain',
+			'11d':'wi-thunderstorm',
+			'13d':'wi-snow',
+			'50d':'wi-fog',
+			'01n':'wi-night-clear',
+			'02n':'wi-night-cloudy',
+			'03n':'wi-night-cloudy',
+			'04n':'wi-night-cloudy',
+			'09n':'wi-night-showers',
+			'10n':'wi-night-rain',
+			'11n':'wi-night-thunderstorm',
+			'13n':'wi-night-snow',
+			'50n':'wi-night-alt-cloudy-windy'
+		}
 		$.getJSON(currWeatherUrl, currWeatherParams, function(json, t) {
 			var temp = roundVal(json.main.temp) + "&deg;";
-			$(".temp").updateWithText(temp, 1000);
+			var temp_min = roundVal(json.main.temp_min);
+			var temp_max = roundVal(json.main.temp_max);
+			var wind = roundVal(json.wind.speed);
+			var iconClass = iconTable[json.weather[0].icon];
+			var icon = $('<span/>').addClass('icon').addClass('dimmed').addClass('wi').addClass('iconClass');
+			$(".temp").updateWithText(icon.outerHTML() + temp, 1000);
+
+			var now = new Date();
+			var sunrise = new Date(json.sys.sunrise * 1000).toTimeString().substring(0, 5);
+			var sunset = new Date(json.sys.sunset * 1000).toTimeString().substring(0, 5);
+			var windString = '<span class="wi wi-strong-wind xdimmed"></span' + wind;
+			var sunString = '<span class="wi wi-sunrise xdimmed"></span>' + sunrise;
+			if (json.sys.sunrise * 1000 < now && json.sys.sunset * 1000 > now) {
+				sunString = '<span class="wi wi-sunset xdimmed"></span>' + sunset;
+			}
+			$(".windsun").updateWithText(windString + ' ' + sunString, 1000);
 		});
 	}, 60000);
 });
